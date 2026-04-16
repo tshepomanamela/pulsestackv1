8dc862e7e87c7417cddb65c91d5965d2b95bc833
Tshepo Manamela <mnmtsh023@myuct.ac.za>
2026-04-16 15:58:07 +0200

    fix(terraform): correct provider block and module source paths

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,35 @@
-terraform{
+terraform {
+  required_version = ">= 1.6"
 
- required_version = ">=1.6"
- required_providers {
-   source= "hashicorp/azurerm"
-   version="~> 3.0"
- }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 
-backend "azurerm" {
-    resource_group_name = "pulsestack-rg"
+  backend "azurerm" {
+    resource_group_name  = "pulsestack-rg"
     storage_account_name = "pulsestacktfstate"
-    container_name = "tfstate"
-    key = "pulsestack.tfstate"
-  
- }
+    container_name       = "tfstate"
+    key                  = "pulsestack.tfstate"
+  }
 }
+
 provider "azurerm" {
   features {}
-
 }
 
 module "networking" {
-  source = ".modules/networking"
+  source              = "./modules/networking"
   resource_group_name = var.resource_group_name
-  location = var.location
-
+  location            = var.location
 }
 
 module "compute" {
-  source = ".modules/compute"
+  source              = "./modules/compute"
   resource_group_name = var.resource_group_name
-  location = var.location
-  subnet_id = module.networking.subnet_id
-  admin_ssh_key = var.admin_ssh_key
-
+  location            = var.location
+  subnet_id           = module.networking.subnet_id
+  admin_ssh_key       = var.admin_ssh_key
 }
